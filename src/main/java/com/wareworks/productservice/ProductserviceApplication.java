230d6bc543f6ef3a8c29a30e60faeeb3afb746c0
amoolekan <146351654+amoolekan@users.noreply.github.com>
2024-09-06 23:15:06 +0100

    Update ProductserviceApplication.java

--- src/main/java/com/wareworks/productservice/ProductserviceApplication.java
+++ src/main/java/com/wareworks/productservice/ProductserviceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductserviceApplication {
+public class ProductserviceApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductserviceApplication.class, args);
